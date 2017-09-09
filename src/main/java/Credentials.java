2ef0146871c455a5b47b7182867b5eba2ad547fd
Tiger Wang <theneewpotato@gmail.com>
2017-09-09 11:05:43 -0500

    Initial commit

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,54 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Credentials {
+
+    public HttpClient httpClient;
+    public HttpContext httpContext;
+
+    public Credentials(String districtCode, String userName, String password, String userType) {
+        CookieStore cookieStore = new BasicCookieStore();
+        HttpClientBuilder builder = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).setRedirectStrategy(new LaxRedirectStrategy());
+        httpClient = builder.build();
+        httpContext = new BasicHttpContext();
+        // post/login init
+        HttpPost httpPost = new HttpPost("https://tws-tn.client.renweb.com/pw/");
+        httpPost.setHeader("Referer", "https://tws-tn.client.renweb.com/pw/");
+        List<NameValuePair> params = new ArrayList<>(4);
+        params.add(new BasicNameValuePair("DistrictCode", districtCode));
+        params.add(new BasicNameValuePair("UserName", userName));
+        params.add(new BasicNameValuePair("Password", password));
+        params.add(new BasicNameValuePair("UserType", userType));
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // perform post/login
+        HttpResponse httpResponse;
+        try {
+            httpResponse = httpClient.execute(httpPost);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+        httpContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+        System.out.println("login: " + httpResponse.getStatusLine());
+    }
+
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,22 @@
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class Test {
+    public static void main(String[] args) {
+        Credentials credentials = new Credentials(
+                "TWS-TN",
+                "thenewpotato",
+                "20020320hua",
+                "PARENTSWEB-STUDENT");
+
+        Schedule schedule = new Schedule(credentials);
+        for (Course course : schedule.get(Schedule.FRIDAY)) {
+            System.out.println("In " + course.location + " for " + course.name + " at " + course.startTime + "-" + course.endTime);
+        }
+        
+        // grade book
+
+        // homework
+    }
+}
